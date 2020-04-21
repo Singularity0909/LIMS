b9abf26bc175401163285f736ff9139aed342206
Singularity0909 <904854724@qq.com>
2020-04-21 18:31:40 +0800

    update

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 
 @section('content')
-  <div class="jumbotron">
+  <div class="jumbotron welcome">
     <h2>Welcome to Library Information Management System</h2>
   </div>
 @stop
