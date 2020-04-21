d787386907d61a04ff7b7a8e9ca217b20a02fb33
Singularity0909 <904854724@qq.com>
2020-04-21 15:18:27 +0800

    background

--- public/js/app.js
+++ public/js/app.js
@@ -37119,8 +37119,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/2U/Code/weibo/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/2U/Code/weibo/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/2U/Code/LIMS/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/2U/Code/LIMS/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
