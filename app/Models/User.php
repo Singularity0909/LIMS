007682ac60f459aabe142a356d947bfe92ef282d
Singularity0909 <904854724@qq.com>
2020-04-21 15:35:36 +0800

    https gravatar

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
-        return "http://www.gravatar.com/avatar/$hash?s=$size";
+        return "https://www.gravatar.com/avatar/$hash?s=$size";
     }
 
     public static function getRole(User $user)
