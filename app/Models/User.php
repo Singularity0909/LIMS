56d5b7b19481e4074056b3a31ecbc6b5080d2504
Singularity0909 <904854724@qq.com>
2020-05-31 00:47:56 +0800

    change gravatar image source

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,7 +44,7 @@ class User extends Authenticatable
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
-        return "https://www.gravatar.com/avatar/$hash?s=$size";
+        return "https://cdn.v2ex.com/gravatar/$hash?s=$size";
     }
 
     public static function getRole(User $user)
